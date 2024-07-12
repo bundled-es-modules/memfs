4775ea4f632c90a1aaa517d86de040c6aaf5683b
jorenbroekema <joren.broekema@gmail.com>
2024-07-12 15:05:52 +0200

    fix: re-export named exports in ESM bundle as well, fix license

--- source.js
+++ source.js
@@ -1,2 +1,13 @@
-import memfs from "memfs";
-export default memfs;
+import memfsDefault, {
+  memfs as _memfs,
+  fs as _fs,
+  createFsFromVolume as _createFsFromVolume,
+  vol as _vol,
+  Volume as _Volume,
+} from "memfs";
+export default memfsDefault;
+export const memfs = _memfs;
+export const fs = _fs;
+export const createFsFromVolume = _createFsFromVolume;
+export const vol = _vol;
+export const Volume = _Volume;
